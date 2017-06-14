fa3dabf4e09acb89ee9dbfce7a2518cab625a1c1
Thomas Laporte <thomas.laporte03@gmail.com>
2017-06-14 14:20:42 +0200

    Add files via upload

---
+++ creationWPDocker.sh
@@ -0,0 +1,3 @@
+# Creation d'un script qui crée deux containers qui contiendront une version propre de Wordpress 
+#			- Un container Apache, un container MySQL
+#           - Utilisation wpcli (permet de faire une install de wordpress en ligne de commande)
